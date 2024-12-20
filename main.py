46658e73a34316ad088930cc467050c16f0ca33e
snowyiy <enek54363027@gmail.com>
2024-12-20 20:36:10 +0100

    add a few buttons and a text area

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ class LetterStats:
         self.dark_theme = True
 
         # image
-        self.img_dark_light_switch_button = ctk.CTkImage(light_image=Image.open("./assets/sun.png"), dark_image=Image.open("./assets/moon.png"))
+        self.img_dark_light_switch_button = ctk.CTkImage(light_image=Image.open("./assets/sun.png"),
+                                                         dark_image=Image.open("./assets/moon.png"))
+        self.img_info = ctk.CTkImage(Image.open("./assets/info.png"))
+        self.img_settings = ctk.CTkImage(Image.open("./assets/setting.png"))
 
 
     def drawGraph(self):
@@ -20,6 +23,14 @@ class LetterStats:
     def calculFreq(self):
         pass
     
+
+    def showInfos(self):
+        pass
+
+
+    def showSettings(self):
+        pass
+
 
     def switchTheme(self):
         self.dark_theme = not self.dark_theme
@@ -33,12 +44,51 @@ class LetterStats:
         root = ctk.CTk()
         root.title("Letter Stats")
         ctk.set_appearance_mode("dark")
-        root.geometry("1080x720")
+        root.geometry("900x600")
+        root.resizable(width=False, height=False)   # Optional (not to fix my laziness but a feature)
         root.grid_columnconfigure(0, weight=1)
         ctk.deactivate_automatic_dpi_awareness()
         
 
-        btn_switch_theme = ctk.CTkButton(root, text="", image=self.img_dark_light_switch_button, fg_color="#aca6a6", hover_color="#c5bcbc", command=self.switchTheme).grid(row=0, column=10, padx=10, pady=10)
+        btn_switch_theme = ctk.CTkButton(root,
+                                         text="",
+                                         width=20,
+                                         height=20,
+                                         image=self.img_dark_light_switch_button,
+                                         fg_color="#aca6a6",
+                                         hover_color="#c5bcbc",
+                                         command=self.switchTheme
+        ).place(x=860, y=10)
+
+        btn_settings = ctk.CTkButton(root,
+                                    text="",
+                                    width=20,
+                                    height=20,
+                                    image=self.img_settings,
+                                    fg_color="#aca6a6",
+                                    hover_color="#c5bcbc",
+                                    command=self.showSettings
+        ).place(x=10, y=10)
+        
+        btn_info = ctk.CTkButton(root,
+                                text="",
+                                width=20,
+                                height=20,
+                                image=self.img_info,
+                                fg_color="#aca6a6",
+                                hover_color="#c5bcbc",
+                                command=self.showInfos
+        ).place(x=60, y=10)
+
+
+
+        txt_text_box = ctk.CTkTextbox(root,
+                                      height=425, width=350, 
+                                      border_width=4,
+                                      border_color="black",
+                                      font=("Arial", 16)
+        ).place(x=25, y=75)
+
 
 
 
