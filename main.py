1be58d6d9a86b69ab4b0de854373efc799cc01bc
snowyiy <enek54363027@gmail.com>
2024-12-22 00:08:12 +0100

    now show a histogram wich shows the frequence of the letters inthe text area

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ class LetterStats:
         self.dark_theme = True
         self.default_dark_theme = ctk.StringVar()
         self.version = "0.0.1"   # get from config
+        self.repo_url = "https://github.com/snowyiy/LetterStats"  # get from config
+        self.dictionnary = {"a" : 0.0, "z" : 0.0, "e" : 0.0, "r" : 0.0, "t" : 0.0, "y" : 0.0, "u" : 0.0, "i" : 0.0, "o" : 0.0, "p" : 0.0, "q" : 0.0, "s" : 0.0, "d" : 0.0, "f" : 0.0, "g" : 0.0, "h" : 0.0,
+                            "j" : 0.0, "k" : 0.0, "l" : 0.0, "m" : 0.0, "w" : 0.0, "x" : 0.0, "c" : 0.0, "v" : 0.0, "b" : 0.0, "n" : 0.0, "é" : 0.0, "ç" : 0.0, "è" : 0.0, "à" : 0.0, "ù" : 0.0, "ê" : 0.0}
+
+        self.txt_text_box = ctk.CTkTextbox(self.root,
+                                      height=425, width=350, 
+                                      border_width=4,
+                                      border_color="black",
+                                      wrap="word",
+                                      font=("Monospace", 16)
+        )
 
         # image
         self.img_dark_light_switch_button = ctk.CTkImage(light_image=Image.open("./assets/sun.png"),
@@ -22,15 +33,33 @@ class LetterStats:
 
 
     def drawGraph(self):
-        pass
+        x = np.arange(len(self.dictionnary))
+        #plt.hist(x, density=True, bins=30)
+        plt.bar(x, height=self.dictionnary.values())
+        plt.xticks(x, self.dictionnary.keys())
+        plt.ylabel("Frequence")
+        plt.xlabel("Letters")
+        plt.show()
 
 
     def calculFreq(self):
-        pass
-    
+        text = self.txt_text_box.get("0.0", "end")
+        
+        for letter in text:
+            if (letter in self.dictionnary.keys()):
+                self.dictionnary[letter] += 1.0
+
+        for letter in self.dictionnary:
+            self.dictionnary[letter] /= len(text)
+
+
+    def drawFromTextArea(self):
+        self.calculFreq()
+        self.drawGraph()
+
 
     def openGithubPage(self):
-        webbrowser.open_new_tab("https://github.com/snowyiy/LetterStats")
+        webbrowser.open_new_tab(self.repo_url)
         
 
     def showInfos(self):
@@ -128,14 +157,13 @@ class LetterStats:
         ).place(x=60, y=10)
 
 
+        self.txt_text_box.place(x=25, y=75)
 
-        txt_text_box = ctk.CTkTextbox(self.root,
-                                      height=425, width=350, 
-                                      border_width=4,
-                                      border_color="black",
-                                      font=("Arial", 16)
-        ).place(x=25, y=75)
-
+        btn_calcul_freq_text_area = ctk.CTkButton(self.root, 
+                                                  text="Calcul Frequance From Text",
+                                                  command=self.drawFromTextArea,
+                                                  font=("Monospace", 16)
+        ).place(x=60, y=525)
 
 
 
