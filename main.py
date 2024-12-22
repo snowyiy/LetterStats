9d9e7eb0b83a82f7970bf728805d1866a3d6b9e8
snowyiy <enek54363027@gmail.com>
2024-12-22 13:57:09 +0100

    improved a few things and add the posibility to scan files and add a menu for settings and infos (settings still do nothing)

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import webbrowser
+import sqlite3
+from CTkMessagebox import CTkMessagebox
+import json
+import os
 
 
 class LetterStats:
@@ -12,8 +16,9 @@ class LetterStats:
         self.letter_freq = []
         self.dark_theme = True
         self.default_dark_theme = ctk.StringVar()
-        self.version = "0.0.1"   # get from config
-        self.repo_url = "https://github.com/snowyiy/LetterStats"  # get from config
+        self.settings_file = "settings.json"
+        self.version = None   # get from config
+        self.repo_url = None   # get from config
         self.dictionnary = {"a" : 0.0, "z" : 0.0, "e" : 0.0, "r" : 0.0, "t" : 0.0, "y" : 0.0, "u" : 0.0, "i" : 0.0, "o" : 0.0, "p" : 0.0, "q" : 0.0, "s" : 0.0, "d" : 0.0, "f" : 0.0, "g" : 0.0, "h" : 0.0,
                             "j" : 0.0, "k" : 0.0, "l" : 0.0, "m" : 0.0, "w" : 0.0, "x" : 0.0, "c" : 0.0, "v" : 0.0, "b" : 0.0, "n" : 0.0, "é" : 0.0, "ç" : 0.0, "è" : 0.0, "à" : 0.0, "ù" : 0.0, "ê" : 0.0}
 
@@ -32,9 +37,9 @@ class LetterStats:
         self.img_settings = ctk.CTkImage(Image.open("./assets/setting.png"))
 
 
-    def drawGraph(self):
+    def drawGraph(self) -> None:
         x = np.arange(len(self.dictionnary))
-        #plt.hist(x, density=True, bins=30)
+
         plt.bar(x, height=self.dictionnary.values())
         plt.xticks(x, self.dictionnary.keys())
         plt.ylabel("Frequence")
@@ -42,9 +47,7 @@ class LetterStats:
         plt.show()
 
 
-    def calculFreq(self):
-        text = self.txt_text_box.get("0.0", "end")
-        
+    def calculFreq(self, text) -> None:
         for letter in text:
             if (letter in self.dictionnary.keys()):
                 self.dictionnary[letter] += 1.0
@@ -52,17 +55,43 @@ class LetterStats:
         for letter in self.dictionnary:
             self.dictionnary[letter] /= len(text)
 
+        
+    def checkLettersCount(self, text) -> bool:
+        size = 0
+        for i in text:
+            if (i in self.dictionnary.keys()):
+                size += 1
+        if (size < 150):
+            CTkMessagebox(title="Error ! Not Enough Characters",
+                          message="There must be at least 150 characters",
+                          icon="cancel",
+                          font=("Monospace", 16))
+            return False
+        else:
+            return True
 
-    def drawFromTextArea(self):
-        self.calculFreq()
-        self.drawGraph()
+
+    def drawFromTextArea(self) -> None:
+        text = self.txt_text_box.get("0.0", "end")
+
+        if (self.checkLettersCount(text)):
+            self.calculFreq(text)
+            self.drawGraph()
+
+    def drawFromFile(self) -> None:
+        file_text = filedialog.askopenfilename()
+        text = open(file_text, "r+").read()
+
+        if (self.checkLettersCount(text)):
+            self.calculFreq(text)
+            self.drawGraph()
 
 
-    def openGithubPage(self):
+    def openGithubPage(self) -> None:
         webbrowser.open_new_tab(self.repo_url)
         
 
-    def showInfos(self):
+    def showInfos(self) -> None:
         top_infos = ctk.CTkToplevel(self.root)
         top_infos.title("Infos")
         top_infos.resizable(height=False, width=False)
@@ -84,7 +113,7 @@ class LetterStats:
         ).pack(anchor=ctk.CENTER, pady=10)
 
 
-    def setDefaultTheme(self):
+    def setDefaultTheme(self) -> None:
         if (self.default_dark_theme == "on"):
             self.dark_theme = True
         else:
@@ -93,7 +122,7 @@ class LetterStats:
         #! EDIT CONFIG FILE
 
 
-    def showSettings(self):
+    def showSettings(self) -> None:
         top_settings = ctk.CTkToplevel(self.root)
         top_settings.title("Settings")
         top_settings.resizable(height=False, width=False)
@@ -109,7 +138,7 @@ class LetterStats:
         ).pack(anchor=ctk.CENTER, pady=60)
 
 
-    def switchTheme(self):
+    def switchTheme(self) -> None:
         self.dark_theme = not self.dark_theme
         if (self.dark_theme):
             ctk.set_appearance_mode("dark")
@@ -117,7 +146,33 @@ class LetterStats:
             ctk.set_appearance_mode("light")
 
 
-    def main(self):
+    def loadConfig(self):
+        if os.path.exists(self.settings_file):
+            with open(self.settings_file, "r+") as config_file:
+                self.version = json.load(config_file)["App"]["version"]
+                self.default_dark_theme = json.load(config_file)["Settings"]["dark_theme"]
+                config_file.close()
+        else:
+            jsonwrite =  {
+                "App": {
+                    "name": "LettersStats",
+                    "version": "0.0.1",
+                    "author": "N&ko",
+                    "Repo": "https://github.com/snowyiy/LetterStats"
+                },
+                "Settings": {
+                    "dark_theme": true
+                }
+            }
+
+            with open(self.settings_file, "w+") as config_file:
+                json.dump(jsonwrite, config_file)
+                config_file.close()
+
+        # TODO change config file to write default dark theme
+
+
+    def main(self) -> None:
         self.root.title("Letter Stats")
         ctk.set_appearance_mode("dark")
         self.root.geometry("900x600")
@@ -165,6 +220,11 @@ class LetterStats:
                                                   font=("Monospace", 16)
         ).place(x=60, y=525)
 
+        btn_calcul_freq_file = ctk.CTkButton(self.root, 
+                                            text="Calcul Frequance From File",
+                                            command=self.drawFromFile,
+                                            font=("Monospace", 16)
+        ).place(x=460, y=250)
 
 
         self.root.mainloop()
